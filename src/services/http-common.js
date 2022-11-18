c0c949945dea6cc131355dad0feec838c90e0b06
Andrew Gay <58545642+andrwmg@users.noreply.github.com>
2022-11-17 23:39:37 -0800

    initial deployment

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const loc = window.location
+
 export default axios.create({
-  baseURL: "http://localhost:8080/data",
+  baseURL: `${loc.protocol}//${loc.hostname}${loc.hostname === 'localhost' ? ':8080/data' : ''}`,
+
+  //  http://localhost:8080/data",
+
   headers: {  "Access-Control-Allow-Origin": "http://localhost:8081"
 }
 });
